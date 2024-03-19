8ac9ca82c3cc751dcd0ad37843828cbaf9ce126c
“lichtle” <leticia.negreiros@outlook.com>
2024-03-19 10:07:16 -0300

    feat: contact done

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,67 @@
+import * as S from "./styles";
+
+export function Contact() {
+  return (
+    <S.ContactContainer>
+      <S.ContactInfos className="container">
+        <h2>Contato</h2>
+        <p>
+          Tem alguma dúvida ou sugestão? Entre em contato comigo pelo formulário
+          abaixo ou através dos links de contato à direita!
+        </p>
+        <div>
+          <S.FormContainer>
+            <S.InputContainer>
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" />
+            </S.InputContainer>
+            <S.InputContainer>
+              <label htmlFor="mail">E-mail</label>
+              <input type="email" id="mail" />
+            </S.InputContainer>
+            <S.InputContainer>
+              <label htmlFor="phone">Telefone (com DDD)</label>
+              <input type="tel" id="phone" />
+            </S.InputContainer>
+            <S.InputContainer>
+              <label htmlFor="message">Sua mensagem</label>
+              <textarea id="message" />
+            </S.InputContainer>
+            <button type="submit">Enviar</button>
+          </S.FormContainer>
+          <S.LinksContainer>
+            <S.Link target="_blank" href="https://wa.me/5535987044117" title="">
+              <i className="fa-brands fa-whatsapp" />
+              WhatsApp
+            </S.Link>
+            <S.Link
+              target="_blank"
+              href="mail:leticia.negreiros@outlook.com"
+              title=""
+            >
+              <i className="fa-regular fa-envelope" />
+              E-mail
+            </S.Link>
+            <S.Link
+              target="_blank"
+              href="https://www.linkedin.com/in/leticianegreiros/"
+              title=""
+            >
+              <i className="fa-brands fa-linkedin" />
+              LinkedIn
+              <a></a>
+            </S.Link>
+            <S.Link
+              target="_blank"
+              href="https://www.instagram.com/frontendkitty/"
+              title=""
+            >
+              <i className="fa-brands fa-instagram" />
+              Instagram
+            </S.Link>
+          </S.LinksContainer>
+        </div>
+      </S.ContactInfos>
+    </S.ContactContainer>
+  );
+}
---
+++ src/components/Contact/styles.ts
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const ContactContainer = styled.div`
+  padding: 64px 0;
+`;
+
+export const ContactInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  gap: 64px;
+
+  h2,
+  p {
+    text-align: center;
+    width: 70%;
+  }
+
+  > div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  width: 500px;
+
+  label {
+    font-weight: 500;
+  }
+
+  label,
+  button {
+    font-size: 18px;
+  }
+
+  input,
+  textarea,
+  button {
+    padding: 12px;
+    border: 1px solid #000;
+  }
+
+  textarea {
+    resize: none;
+    height: 100px;
+    overflow-y: scroll;
+    border: 1px solid #000;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: #a6cf98;
+    border: none;
+    font-family: "Montserrat Alternates", sans-serif;
+    font-weight: 700;
+    color: #557c55;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const Link = styled.a`
+  color: #000;
+  font-size: 28px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    &:nth-child(odd) {
+      color: #557c55;
+    }
+
+    &:nth-child(even) {
+      color: #fa7070;
+    }
+  }
+`;
