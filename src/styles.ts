545ff7f611753e31cd65c56741fd687c0a8ae10f
“lichtle” <leticia.negreiros@outlook.com>
2024-03-19 10:07:37 -0300

    feat: global style done

---
+++ src/styles.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+		box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  body {
+    background-color: #F2FFE9;
+    font-family: "Montserrat Alternates", sans-serif;
+  }
+
+  h1, h2 {
+    font-weight: 500;
+    font-size: 56px;
+  }
+
+  p {
+    font-size: 22px;
+  }
+  
+  .container {
+    max-width: 1280px;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  
+`;
+
+export default GlobalStyle;
