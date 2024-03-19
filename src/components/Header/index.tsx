b173f85401d5ad31f23f7838464eefa312f99775
“lichtle” <leticia.negreiros@outlook.com>
2024-03-19 18:34:12 -0300

    chore: header responsive done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,19 +6,31 @@ export function Header() {
   return (
     <HeaderContainer className="container">
       <Logo src={logo} alt="Logo" />
-      <nav>
-        <List>
+      <nav className="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label className="menu__btn" htmlFor="menu__toggle">
+          <span></span>
+        </label>
+        <List className="menu__box">
           <li>
-            <a href="">Encomendas</a>
+            <a className="menu__item" href="#order">
+              Encomendas
+            </a>
           </li>
           <li>
-            <a href="">Galeria</a>
+            <a className="menu__item" href="#gallery">
+              Galeria
+            </a>
           </li>
           <li>
-            <a href="">Receitas</a>
+            <a className="menu__item" href="">
+              Receitas
+            </a>
           </li>
           <li>
-            <a href="">Contato</a>
+            <a className="menu__item" href="#contact">
+              Contato
+            </a>
           </li>
         </List>
       </nav>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,20 +2,119 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   display: flex;
-  align-items: center;
   justify-content: space-between;
   padding: 32px 0;
+
+  #menu__toggle {
+    opacity: 0;
+  }
+
+  @media (max-width: 1024px) {
+    padding: 32px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 32px 0;
+    justify-content: space-around;
+
+    #menu__toggle:checked + .menu__btn > span {
+      transform: rotate(45deg);
+    }
+
+    #menu__toggle:checked + .menu__btn > span::before {
+      top: 0;
+      transform: rotate(0deg);
+    }
+
+    #menu__toggle:checked + .menu__btn > span::after {
+      top: 0;
+      transform: rotate(90deg);
+    }
+
+    #menu__toggle:checked ~ .menu__box {
+      right: 0;
+    }
+
+    .menu__btn {
+      position: fixed;
+      top: 38px;
+      right: 16px;
+      width: 26px;
+      height: 26px;
+      cursor: pointer;
+      z-index: 1;
+      background-color: #f2ffe9;
+      padding: 22px;
+      border-radius: 24px;
+    }
+
+    .menu__btn > span,
+    .menu__btn > span::before,
+    .menu__btn > span::after {
+      display: block;
+      position: absolute;
+      width: 26px;
+      right: 9px;
+      height: 2px;
+      background-color: #000;
+      transition-duration: 0.3s;
+    }
+
+    .menu__btn > span::before {
+      content: "";
+      top: -8px;
+      left: 0;
+    }
+
+    .menu__btn > span::after {
+      content: "";
+      top: 8px;
+      left: 0;
+    }
+
+    .menu__box {
+      display: block;
+      position: fixed;
+      top: 0;
+      right: -100%;
+      width: 250px;
+      height: 100%;
+      margin: 0;
+      padding: 96px 0 0 8px;
+      list-style: none;
+      background-color: #eceff1;
+      box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.7);
+      transition-duration: 0.4s;
+    }
+
+    .menu__item {
+      display: block;
+      padding: 16px 24px;
+      font-size: 22px;
+      font-weight: 500;
+      text-decoration: none;
+      transition-duration: 0.25s;
+    }
+
+    .menu__item:hover {
+      background-color: #cfd8dc;
+    }
+  }
 `;
 
 export const Logo = styled.img`
   width: 300px;
   user-select: none;
+
+  @media (max-width: 576px) {
+    width: 250px;
+  }
 `;
 
 export const List = styled.div`
   display: flex;
   align-items: center;
-  gap: 60px;
+  gap: 64px;
 
   a {
     display: inline-block;
@@ -25,21 +124,29 @@ export const List = styled.div`
     color: #000;
   }
 
+  @media (max-width: 1024px) {
+    gap: 32px;
+
+    a {
+      font-size: 22px;
+    }
+  }
+
   a::after {
     content: "";
     position: absolute;
     bottom: 0;
-    left: 50%;
+    right: 50%;
     display: block;
     background: none repeat scroll 0 0 transparent;
     height: 3px;
     width: 0;
     background: #fa7070;
-    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    transition: width 0.3s ease 0s, right 0.3s ease 0s;
   }
 
   a:hover::after {
     width: 100%;
-    left: 0;
+    right: 0;
   }
 `;
